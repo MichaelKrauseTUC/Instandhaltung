33a4a1152a460284b753b01c306e75d9a7ec8d69
MichaelMKKrause <Michael.Krause@tu-clausthal.de>
2018-02-19 11:19:31 +0100

    zusammenführen von Java Code, den ich habe - läuft wenigstens, nur nicht mehr richtig

---
+++ src/main/java/com/krause/instandhaltung/CAlgorithmus2.java
@@ -0,0 +1,9 @@
+package com.krause.instandhaltung;
+
+public class CAlgorithmus2 {
+    public CAlgorithmus2(int i, CSystem cSystem) {
+    }
+
+    public void run() {
+    }
+}
--- src/main/java/com/krause/instandhaltung/ISystem.java
+++ src/main/java/com/krause/instandhaltung/ISystem.java
@@ -1,3 +1,3 @@
 package com.krause.instandhaltung;
 
-public interface ISystem{}
+interface ISystem{}
