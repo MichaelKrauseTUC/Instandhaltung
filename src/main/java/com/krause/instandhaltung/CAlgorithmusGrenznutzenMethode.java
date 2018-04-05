495365fca12fe98e50ca1a865911401a2bfc1667
MichaelMKKrause <Michael.Krause@tu-clausthal.de>
2018-04-05 15:30:56 +0200

    Versuch, Algorithmus von Herrn Schwindt umzusetzen
    wegen Verschleiss jetzt anderer Ansatz (With-Without)

--- src/main/java/com/krause/instandhaltung/CAlgorithmusGrenznutzenMethode.java
+++ src/main/java/com/krause/instandhaltung/CAlgorithmusGrenznutzenMethode.java
@@ -18,12 +18,12 @@ import cern.colt.matrix.impl.DenseDoubleMatrix1D;
  */
 public class CAlgorithmusGrenznutzenMethode extends AAlgorithmus {
 
-	private double granularitaet = 0.01;
+	public static final double GRANULARITAET = 0.01;
 	private CZustand zustand;
 	private int zeit;
 	private int anzahlIterationen;
 	private double gesamtBudget;
-	private ArrayList<Double> anfangsLeistung;
+	private Double[] anfangsLeistung;
 
 	private int anzKomponenten;
 	private DoubleMatrix1D invs;
@@ -43,25 +43,29 @@ public class CAlgorithmusGrenznutzenMethode extends AAlgorithmus {
 
 	@Override
 	public void initialisieren() {
-		anfangsLeistung = new ArrayList<>();
-		CKomponente c1 = new CKomponente(new CKonstanterVerschleiss(0.4), new CKonkaverInvestEinflussExponential(5),1.0);
-		CKomponente c2 = new CKomponente(new CKonstanterVerschleiss(0.4), new CKonkaverInvestEinflussExponential(4),1.0);
-		CKomponente c3 = new CKomponente(new CKonstanterVerschleiss(0.3), new CKonkaverInvestEinflussExponential(6),0.9);
+
+		CKomponente c1 = new CKomponente(new CKonstanterVerschleiss(0), new CKonkaverInvestEinflussExponential(0.39),
+				0.9);
+		CKomponente c2 = new CKomponente(new CKonstanterVerschleiss(0), new CKonkaverInvestEinflussExponential(4),
+				0.9);
+		CKomponente c3 = new CKomponente(new CKonstanterVerschleiss(0), new CKonkaverInvestEinflussExponential(4.1),
+				0.9);
 		komponenten = new ArrayList<>();
 		komponenten.add(c1);
 		komponenten.add(c2);
 		komponenten.add(c3);
-		for (IKomponente komp : komponenten) {
-			komp.setLeistung(anfangsLeistung.get(komponenten.indexOf(komp)));
-		}
+
 		gesamtBudget = 1.0;
 		serSys = new CSerienSystem(komponenten);
 		this.zustand = new CZustand(gesamtBudget, serSys);
+
 		anzKomponenten = zustand.getSystem().getKomponenten().size();
+		anfangsLeistung = new Double[anzKomponenten];
 		for (int i = 0; i < anzKomponenten; i++) {
-			anfangsLeistung.add(komponenten.get(i).getLeistung());
+			anfangsLeistung[i] = komponenten.get(i).getLeistung();
 		}
 		invs = new DenseDoubleMatrix1D(anzKomponenten);
+		invs.assign(0);
 		lsgHistory = new ArrayList<>();
 		grenznutzen = new Double[anzKomponenten];
 		komponentenListeCPlus = new ArrayList<>();
@@ -69,46 +73,62 @@ public class CAlgorithmusGrenznutzenMethode extends AAlgorithmus {
 
 	@Override
 	public void ausfuehren() {
-		grenznutzenBerechnen(granularitaet);
-		int maxArgGrenznutzen=-1;
-		double maxGrenznutzen=Double.MIN_VALUE;
+		grenznutzenBerechnen(GRANULARITAET);
+		int maxArgGrenznutzen = -1;
+		double maxGrenznutzen = -Double.MAX_VALUE;
 		for (int i = 0; i < grenznutzen.length; i++) {
-			if (grenznutzen[i]>maxGrenznutzen) {
-				maxGrenznutzen=grenznutzen[i];
-				maxArgGrenznutzen=i;
+			if (grenznutzen[i] > maxGrenznutzen) {
+				maxGrenznutzen = grenznutzen[i];
+				maxArgGrenznutzen = i;
 			}
 		}
 		komponentenListeCPlus.add(maxArgGrenznutzen);
-		while (gesamtBudget>0) {
-			if (komponentenListeCPlus.size()<anzKomponenten)
-			{
-				zweitMaxArgGrenznutzen=-1;
-				zweitMaxGrenznutzen=Double.MIN_VALUE;
+		while (gesamtBudget > 0) {
+			double b = 0;
+			if (komponentenListeCPlus.size() <= anzKomponenten) {
+				zweitMaxArgGrenznutzen = -1;
+				zweitMaxGrenznutzen = -Double.MAX_VALUE;
 				for (int i = 0; i < grenznutzen.length; i++) {
 					if (komponentenListeCPlus.contains(i))
 						break;
-					if (grenznutzen[i]>zweitMaxGrenznutzen) {
-						zweitMaxGrenznutzen=grenznutzen[i];
-						zweitMaxArgGrenznutzen=i;
+					if (grenznutzen[i] > zweitMaxGrenznutzen) {
+						zweitMaxGrenznutzen = grenznutzen[i];
+						zweitMaxArgGrenznutzen = i;
 					}
 				}
-				double b = kleinstesBBestimmen();
+				b = kleinstesBBestimmen(gesamtBudget);
 			}
+			if ((komponentenListeCPlus.size() == anzKomponenten) || (b > gesamtBudget)) {
+				b = gesamtBudget;
+			} else
+				komponentenListeCPlus.add(zweitMaxArgGrenznutzen);
+			gesamtBudget -= b;
 		}
 		System.out.println(maxGrenznutzen);
 	}
-/**
- * @todo Methode implementieren
- * @return
- */
-	private double kleinstesBBestimmen() {
-		double b=0;
+
+	/**
+	 * @return kleinstes b, dass die Ableitung des maximalen Grenznutzens nach unten
+	 *         auf den zweitniedrigsten Wert drueckt
+	 */
+	private double kleinstesBBestimmen(double maxBudget) {
+		double b = 0;
 		for (int j = 0; j < komponentenListeCPlus.size(); j++) {
 			int i = komponentenListeCPlus.get(j);
-			invs.set(i, invs.get(i)+granularitaet);
-			
+			while (true) {
+				if (b + GRANULARITAET <= maxBudget) {
+//					if (b >= 0.01)
+//						System.out.println("jetzt gucken");
+					invs.set(i, invs.get(i) + GRANULARITAET);
+					b += GRANULARITAET;
+					double grenzNutzenEinzel = grenznutzenBerechnenEinzel(komponenten.get(i), invs.get(i));
+					if ((zweitMaxGrenznutzen <= grenzNutzenEinzel + CMain.FEHLERTOLERANZ)
+							&& (zweitMaxGrenznutzen >= grenzNutzenEinzel - CMain.FEHLERTOLERANZ))
+						break;
+				} else
+					break;
+			}
 		}
-		
 		return b;
 	}
 
@@ -117,9 +137,17 @@ public class CAlgorithmusGrenznutzenMethode extends AAlgorithmus {
 		for (int i = 0; i < anzKomponenten; i++) {
 			komponenten.get(i).zeitschrittDurchfuehren(inv);
 			double zfwNachher = serSys.strukturfunktionBerechnen();
-			grenznutzen[i]= zfwNachher - zfwVorher;
+			grenznutzen[i] = (zfwNachher - zfwVorher)/inv;
 			komponenten.get(i).leistungSchrittZurueck();
 		}
+	}
+
+	private double grenznutzenBerechnenEinzel(IKomponente komp, double inv) {
+		double zfwVorher = serSys.strukturfunktionBerechnen();
+		komp.zeitschrittDurchfuehren(inv);
+		double zfwNachher = serSys.strukturfunktionBerechnen();
+		komp.leistungSchrittZurueck();
+		return ((zfwNachher - zfwVorher)/inv);
 	}
 
 	@Override
